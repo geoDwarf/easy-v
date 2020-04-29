981cc2f696c6b44c016c4b25a4f084f8dfe38bcd
geoDwarf <fedo.baiocco@gmail.com>
2020-04-29 18:36:11 +0100

    this probably need to be reverted

--- src/main/java/com/geodwarf/App.java
+++ src/main/java/com/geodwarf/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "I don t want to say hello world any more!" );
     }
 }
